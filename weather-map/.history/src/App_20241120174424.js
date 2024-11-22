8d411513d08f03b18c43874bdd1fe7331d6241d0
Mahsa Javadi <mahsa.javady@gmail.com>
2024-11-22 09:59:44 -0500

    Remove .history folder and add to .gitignore

--- weather-map/.history/src/App_20241120174424.js
+++
@@ -1,8 +0,0 @@
-import './App.css';
-import axios from 'axios';
-
-function App() {
-  return <div className="App">app</div>;
-}
-
-export default App;
--- weather-map/.history/src/App_20241120175207.js
+++
@@ -1,9 +0,0 @@
-import './App.css';
-import axios from 'axios';
-
-function App() {
-  const api = `https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}`;
-  return <div className="App">app</div>;
-}
-
-export default App;
